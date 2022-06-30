fc84fa2e6b02fda0a1ccdb34370000cc12ea36d3
Nicola Rossi <nicolarossi1002@gmail.com>
2022-06-30 23:03:14 +0200

    Primo commit

---
+++ src/test/java/co/develhope/MockDatabase/MockDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.MockDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
